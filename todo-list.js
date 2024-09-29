61acea4bd45b18b6a92698e396b07200586d0a9b
Aanya <aanyashukla67@gmail.com>
2024-09-29 22:38:23 +0530

    Added the date feature and did some styling through css.

--- todo-list.js
+++ todo-list.js
@@ -1,11 +1,51 @@
-const todoList = [];
+const todoList = [{
+  name: `make dinner`,
+  dueDate: `2024-12-30`
+}, {
+  name:`wash dishes`,
+  dueDate: `2024-10-05`
+}]; 
 
+renderTodoList();
+
+function renderTodoList(){
+  let todoListHTML = '';
+
+  for(let i = 0; i < todoList.length; i++){
+    const todoObject = todoList[i];
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    const {name, dueDate} = todoObject;
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="
+        todoList.splice(${i}, 1);   
+        renderTodoList();
+      " class="delete-todo-button">Delete</button>
+    `;
+    //splice() removes an element. It takes two values, first is the index which we want to remve and 2nd is the no. of values we want to remove starting from that particular index.
+    todoListHTML += html;
+  }
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+ 
 function addTodo(){
-  const inputElement = document.querySelector('js-name-input');
+  const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
 
-  todoList.push(name);
-  console.log(todoList);
+  const dateInputElement = document.querySelector(`.js-due-date-input`);
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    // name: name,
+    // dueDate: dueDate
+    name,
+    dueDate 
+  });
 
   inputElement.value = ''; 
+
+  renderTodoList();
 }
